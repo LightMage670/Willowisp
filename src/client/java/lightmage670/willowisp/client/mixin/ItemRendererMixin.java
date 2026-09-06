41b4401159fef3f203b32543596900c89948bdd0
LightMage670 <lightmage670@gmail.com>
2026-09-06 15:25:07 -0600

    Removed Item Renderer Mixin since that was breaking in the pack. Item will no longer be invisible in offhand

--- src/client/java/lightmage670/willowisp/client/mixin/ItemRendererMixin.java
+++ src/client/java/lightmage670/willowisp/client/mixin/ItemRendererMixin.java
@@ -16,6 +16,7 @@ import lightmage670.willowisp.Willowisp;
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
 
+    /*
     @Inject(
         method = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
         at = @At("HEAD"),
@@ -27,4 +28,5 @@ public class ItemRendererMixin {
             ci.cancel(); 
         }
     }
-}
+    */
+} 
